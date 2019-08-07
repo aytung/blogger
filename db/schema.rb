a4eb156fb3fa360eaa22d50191e15f411800a893
Albert Tung <albertytung@gmail.com>
2019-08-07 14:22:34 -0700

    Added a few games

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_04_015011) do
+ActiveRecord::Schema.define(version: 2019_08_07_193137) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_to_name"
+    t.string "image_content_size"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
+    t.string "image_file_name"
+    t.string "image_content_type"
   end
 
   create_table "comments", force: :cascade do |t|
